69d8e0d803d3a1de8d3329aa8b04ff831328b162
juliodumont <julioef.dumont@gmail.com>
2022-08-31 11:21:26 -0300

    Challenge done

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+let bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let urlList = { length: 0 };
+
+const addToUrlList = (url) => {
+  urlList = {
+    ...urlList,
+    [urlList.length + 1]: url,
+    length: urlList.length + 1,
+  };
+};
+
+app.post("/api/shorturl", (req, res) => {
+  const urlRegexTest = /^(https?|http):\/\/[^\s$.?#].[^\s]*$/gm.test(
+    req.body.url
+  );
+  if (urlRegexTest === true) {
+    addToUrlList(req.body.url);
+    res.json({ original_url: req.body.url, short_url: urlList.length });
+  } else {
+    res.json({ error: "invalid url" });
+  }
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:url", (req, res) => {
+  const url = urlList[req.params.url];
+  res.status(301).redirect(url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
